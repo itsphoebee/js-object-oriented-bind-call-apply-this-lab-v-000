cca04a30d00f1c7f7141f81439cf14fc1f6cce41
Phoebe Lau <phoebelau7@gmail.com>
2018-03-27 23:02:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function setThisWithCall(fn, value, argument){
   return fn.call(value, argument);
 }
 
-function setThisWithApply(fn, value, [argument1, argument2]){
-  return fn.apply(value, [argument1, argument2])
+function setThisWithApply(fn, value, args){
+  return fn.apply(value, [args])
 }
 
 function returnNewFunctionOf(fn, value){
   let newFn = fn.bind(value)
-  return newFn
+  return newFn;
 }
